80e9b7c247587abbce61770c3bb1f5a083dd9118
Eva Sabeeh <evas190401@gmail.com>
2025-03-23 21:55:34 +0530

    Curviness corrected

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -57,7 +57,7 @@ function Footer() {
                 <svg
                     className="block md:hidden bottom-0 w-full h-full"
                     xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 1000 500"
+                    viewBox="10 0 700 450"
                     preserveAspectRatio="none"
                 >
                     <path
