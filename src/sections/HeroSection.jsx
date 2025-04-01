11581ed3aa5a8e9ac4e2fefac14c3837647e495f
Eva Sabeeh <evas190401@gmail.com>
2025-04-01 11:38:37 +0530

    Product section changed and banners updated

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -1,7 +1,52 @@
+// import React, { useState, useEffect } from "react";
+// import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+
+// const banners = [
+//     "/FruitRush-Beta1/banners/banner1.png",
+//     "/FruitRush-Beta1/banners/banner2.png",
+//     "/FruitRush-Beta1/banners/banner3.png",
+//     "/FruitRush-Beta1/banners/banner4.png",
+//     "/FruitRush-Beta1/banners/banner5.png",
+// ];
+
+// const buttonStyles = "absolute top-1/2 transform -translate-y-1/2 text-black cursor-pointer p-2 rounded-full hover:bg-opacity-75";
+
+// const NavigationButton = ({ direction, onClick }) => (
+//     <button
+//         className={`${buttonStyles} ${direction === "left" ? "left-2" : "right-2"} p-2 md:p-3 lg:p-4`}
+//         onClick={onClick}
+//     >
+//         {direction === "left" ? <FaChevronLeft className="w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8" /> : <FaChevronRight className="w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8" />}
+//     </button>
+// );
+
+// const HeroSection = () => {
+//     const [currentIndex, setCurrentIndex] = useState(0);
+
+//     useEffect(() => {
+//         const interval = setInterval(() => {
+//             setCurrentIndex((prevIndex) => (prevIndex + 1) % banners.length);
+//         }, 4000);
+//         return () => clearInterval(interval);
+//     }, []);
+
+//     return (
+//         <div className="mx-5 md:mx-10 lg:mx-20 mt-10 lg:mt-7 py-4 md:py-11 lg:py-11 text-white">
+//             <div className="relative flex justify-center">
+//                 <img className="img-fluid rounded-2xl shadow-xl" src={banners[currentIndex]} height={300} width={1200} />
+//                 <NavigationButton direction="left" onClick={() => setCurrentIndex((prev) => (prev === 0 ? banners.length - 1 : prev - 1))} />
+//                 <NavigationButton direction="right" onClick={() => setCurrentIndex((prev) => (prev + 1) % banners.length)} />
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default HeroSection;
+
 import React, { useState, useEffect } from "react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
-const banners = [
+const largeBanners = [
     "/FruitRush-Beta1/banners/banner1.png",
     "/FruitRush-Beta1/banners/banner2.png",
     "/FruitRush-Beta1/banners/banner3.png",
@@ -9,33 +54,77 @@ const banners = [
     "/FruitRush-Beta1/banners/banner5.png",
 ];
 
-const buttonStyles = "absolute top-1/2 transform -translate-y-1/2 text-black cursor-pointer p-2 rounded-full hover:bg-opacity-75";
+const smallBanners = [
+    "/FruitRush-Beta1/banners/mobile-banner1.jpg",
+    "/FruitRush-Beta1/banners/mobile-banner2.jpg",
+    "/FruitRush-Beta1/banners/mobile-banner3.jpg",
+    "/FruitRush-Beta1/banners/mobile-banner4.jpg",
+    "/FruitRush-Beta1/banners/mobile-banner5.jpg",
+];
+
+const buttonStyles =
+    "absolute top-1/2 transform -translate-y-1/2 text-black cursor-pointer p-2 rounded-full hover:bg-opacity-75";
 
 const NavigationButton = ({ direction, onClick }) => (
     <button
         className={`${buttonStyles} ${direction === "left" ? "left-2" : "right-2"} p-2 md:p-3 lg:p-4`}
         onClick={onClick}
     >
-        {direction === "left" ? <FaChevronLeft className="w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8" /> : <FaChevronRight className="w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8" />}
+        {direction === "left" ? (
+            <FaChevronLeft className="w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8" />
+        ) : (
+            <FaChevronRight className="w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8" />
+        )}
     </button>
 );
 
 const HeroSection = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
+    const [banners, setBanners] = useState(largeBanners);
+
+    useEffect(() => {
+        const updateBanners = () => {
+            setBanners(window.innerWidth < 768 ? smallBanners : largeBanners);
+            setCurrentIndex(0);
+        };
+
+        updateBanners();
+        window.addEventListener("resize", updateBanners);
+
+        return () => window.removeEventListener("resize", updateBanners);
+    }, []);
 
     useEffect(() => {
         const interval = setInterval(() => {
             setCurrentIndex((prevIndex) => (prevIndex + 1) % banners.length);
         }, 4000);
         return () => clearInterval(interval);
-    }, []);
+    }, [banners]);
 
     return (
         <div className="mx-5 md:mx-10 lg:mx-20 mt-10 lg:mt-7 py-4 md:py-11 lg:py-11 text-white">
             <div className="relative flex justify-center">
-                <img className="img-fluid rounded-2xl shadow-xl" src={banners[currentIndex]} height={300} width={1200} />
-                <NavigationButton direction="left" onClick={() => setCurrentIndex((prev) => (prev === 0 ? banners.length - 1 : prev - 1))} />
-                <NavigationButton direction="right" onClick={() => setCurrentIndex((prev) => (prev + 1) % banners.length)} />
+                <img
+                    className="img-fluid rounded-2xl shadow-xl"
+                    src={banners[currentIndex]}
+                    height={window.innerWidth < 768 ? 200 : 300}
+                    width={window.innerWidth < 768 ? 400 : 1200}
+                    alt="Banner"
+                />
+                <NavigationButton
+                    direction="left"
+                    onClick={() =>
+                        setCurrentIndex((prev) =>
+                            prev === 0 ? banners.length - 1 : prev - 1
+                        )
+                    }
+                />
+                <NavigationButton
+                    direction="right"
+                    onClick={() =>
+                        setCurrentIndex((prev) => (prev + 1) % banners.length)
+                    }
+                />
             </div>
         </div>
     );
--- src/sections/ProductSection.jsx
+++ src/sections/ProductSection.jsx
@@ -1,70 +1,35 @@
+import React, { useState, useEffect } from "react";
+
 function ProductSection() {
-    const products = [
-        {
-            id: 1,
-            image: "/FruitRush-Beta1/product.png",
-            floatimage: "/FruitRush-Beta1/lemon2.png",
-        },
-        {
-            id: 2,
-            image: "/FruitRush-Beta1/product.png",
-            floatimage: "/FruitRush-Beta1/lemon.png",
-        },
-        {
-            id: 3,
-            image: "/FruitRush-Beta1/product.png",
-            floatimage: "/FruitRush-Beta1/strawberry.png",
-        },
+    const images = [
+        "/FruitRush-Beta1/ProductHome.jpg",
+        "/FruitRush-Beta1/product.jpg"
     ];
 
+    const [currentImage, setCurrentImage] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+        }, 3000);
+
+        return () => clearInterval(interval);
+    }, []);
+
     return (
-        <div className="animate-appear pb-15 mt-25">
-            <div className="relative w-full">
-                <img
-                    src="/FruitRush-Beta1/basil.png"
-                    alt="Floating Image"
-                    className="absolute w-[140px] right-15 bottom-[20px] md:bottom-[20px] lg:bottom-[70px] md:right-30 md:w-[240px] h-auto transform transition duration-300 ease-in-out hover:scale-110"
-                />
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 220">
-                    <path
-                        fill="#b4ebe6"
-                        fillOpacity="1"
-                        d="M0,32L20,37.3C40,43,80,53,120,58.7C160,64,200,64,240,58.7C280,53,320,43,360,58.7C400,75,440,117,480,128C520,139,560,117,600,112C640,107,680,117,720,106.7C760,96,800,64,840,64C880,64,920,96,960,101.3C1000,107,1040,85,1080,69.3C1120,53,1160,43,1200,64C1240,85,1280,139,1320,149.3C1360,160,1400,128,1420,112L1440,96L1440,320L1420,320C1400,320,1360,320,1320,320C1280,320,1240,320,1200,320C1160,320,1120,320,1080,320C1040,320,1000,320,960,320C920,320,880,320,840,320C800,320,760,320,720,320C680,320,640,320,600,320C560,320,520,320,480,320C440,320,400,320,360,320C320,320,280,320,240,320C200,320,160,320,120,320C80,320,40,320,20,320L0,320Z"
+        <div className="animate-appear pb-30 mt-25">
+            <div className="flex justify-center items-center pb-15 w-full">
+                <div className="w-full h-[50%]">
+                    <h3 className="text-[var(--darkgreen)] pb-5 md:pb-10 text-4xl md:text-6xl text-center">
+                        OUR PRODUCT
+                    </h3>
+
+                    <img
+                        src={images[currentImage]}
+                        alt="Product"
+                        className="w-full h-[50%] object-cover transition-opacity duration-1000 ease-in-out"
                     />
-                </svg>
-            </div>
-
-            <div className="flex justify-center items-center pb-15 w-full bg-[var(--green)] px-5">
-                <div className="max-w-screen-lg w-full">
-                    <h3 className="text-[var(--darkgreen)] text-4xl md:text-6xl text-center">OUR PRODUCT</h3>
-
-                    <div className="mt-20 grid grid-cols-1 md:grid-cols-3 gap-15 md:gap-30 mx-5 md:mx-20 p-2">
-
-                        {products.map((product) => (
-                            <div
-                                key={product.id}
-                                className={`relative bg-white px-3 rounded-2xl text-center transform transition duration-300 ease-in-out hover:scale-115 shadow-[0px_3px_5px_1px_rgba(0,_0,_0,_0.8)] ${product.id === 2 ? "py-12 h-64" : "py-6 h-52"
-                                    }`}
-                            >
-                                <img
-                                    src={product.floatimage}
-                                    className={`absolute object-contain z-10 transition-transform duration-300
-                ${product.id === 3 ? "bottom-0 right-10 translate-x-[50%] translate-y-[50%] w-40 h-40 md:w-40 md:h-40 lg:w-50 lg:h-50" : "w-25 h-25 md:w-30 md:h-30 lg:w-30 lg:h-30 top-0 left-0 translate-x-[-40%] translate-y-[-50%]"}
-            `}
-                                />
-                                <div className="flex justify-center">
-                                    <img src={product.image} className="w-20 h-40" />
-                                </div>
-                            </div>
-                        ))}
-                    </div>
                 </div>
-            </div>
-
-            <div className="w-full">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                    <path fill="#b4ebe6" fillOpacity="1" d="M0,96L26.7,101.3C53.3,107,107,117,160,106.7C213.3,96,267,64,320,85.3C373.3,107,427,181,480,192C533.3,203,587,149,640,133.3C693.3,117,747,139,800,122.7C853.3,107,907,53,960,74.7C1013.3,96,1067,192,1120,224C1173.3,256,1227,224,1280,208C1333.3,192,1387,192,1413,192L1440,192L1440,0L1413.3,0C1386.7,0,1333,0,1280,0C1226.7,0,1173,0,1120,0C1066.7,0,1013,0,960,0C906.7,0,853,0,800,0C746.7,0,693,0,640,0C586.7,0,533,0,480,0C426.7,0,373,0,320,0C266.7,0,213,0,160,0C106.7,0,53,0,27,0L0,0Z"></path>
-                </svg>
             </div>
         </div>
     );
